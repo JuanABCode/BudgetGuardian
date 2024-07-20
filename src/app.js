4c4dade421fd70f7b5e8b588ea191a35032288a1
JuanABCode <143307530+JuanABCode@users.noreply.github.com>
2024-07-19 21:28:01 -0700

    Update app.js

--- src/app.js
+++ src/app.js
@@ -2,15 +2,12 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-// Serve static files from the 'public' directory
 app.use(express.static('public'));
 
-// Define a route for the home page
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/public/index.html');
 });
 
-// Start the server
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}`);
 });
